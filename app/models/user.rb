204755e07532fa7084958c6ce879baafcbea9698
Charles Guo <shaldengeki@gmail.com>
2017-09-09 14:54:25 -0700

    User model, GraphQL reader endpoints
    Adds a User model that records usernames and emails, with appropriate validations. Also adds GraphQL endpoints to get a list of all users, or to lookup a specific user by email.
    
    TG-22 #code-review

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+	validates :email,
+		presence: true,
+		format: {
+			with: /.*@.*\..*/,
+			message: "Must be a valid email address"
+		},
+		length: {
+			minimum: 6,
+			maximum: 100
+		}
+
+	validates :username,
+		presence: true,
+		uniqueness: true,
+		length: {
+			minimum: 1,
+			maximum: 50
+		}
+end
