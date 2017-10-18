86977133813ba02278b5f18227ae1a9d53107280
Charles Guo <shaldengeki@gmail.com>
2017-10-17 20:58:23 -0700

    Add Devise to the User model
    Adds Devise facilities to the User model, so that users can sign in.
    Also installs pundit, though no policies are currently implemented.
    
    TG-7 #started

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/"
   end
 
+  root to: "graphql#execute"
   post "/", to: "graphql#execute"
-  get "/", to: "graphql#execute"
 end
